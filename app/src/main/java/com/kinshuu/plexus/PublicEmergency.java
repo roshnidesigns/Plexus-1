3e85a2746321c47c3579b6dddce61c2c9b42c94a
Manthan Surkar <manthan.surkar@gmail.com>
2020-02-01 09:57:48 +0530

    added file changes for commit

--- app/src/main/java/com/kinshuu/plexus/PublicEmergency.java
+++ app/src/main/java/com/kinshuu/plexus/PublicEmergency.java
@@ -3,12 +3,18 @@ package com.kinshuu.plexus;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.telecom.Call;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,12 +24,19 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.Headers;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -32,12 +45,37 @@ import okhttp3.Response;
 public class PublicEmergency extends AppCompatActivity {
 
     EditText title,description;
+    Bitmap bitmap;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        //Detects request codes
+        if(requestCode==1 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+             bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setLogo(R.drawable.ic_navbar);
+        Intent intent = new Intent();
+
+
+
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), 1);
+
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setBackgroundDrawable(new ColorDrawable(0));
@@ -53,7 +91,7 @@ public class PublicEmergency extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_emergency);
         title = (EditText)findViewById(R.id.title);
-        Intent intent = getIntent();
+        Intent intents = getIntent();
         description = (EditText)findViewById(R.id.des);
         Button submitButton = (Button) findViewById(R.id.submit);
         Toast t = Toast.makeText(this.getApplicationContext(), "Public Emergency", Toast.LENGTH_SHORT);
@@ -85,7 +123,7 @@ public class PublicEmergency extends AppCompatActivity {
         tryPost();
 
     }
-
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
     void tryPost(){
 
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -95,12 +133,24 @@ public class PublicEmergency extends AppCompatActivity {
         String url = "http://172.27.49.95/emergency/emergency/public/addPost";
 
 
-        RequestBody body = new FormBody.Builder()
-                .add("title", titles)
-                .add("description", des)
-                .add("name",MainActivity.mUsername)
-                .add("email",MainActivity.mUserEmail)
-                .build();
+//        RequestBody body = new FormBody.Builder()
+//                .add("title", titles)
+//                .add("description", des)
+//                .add("name",MainActivity.mUsername)
+//                .add("email",MainActivity.mUserEmail)
+//                .build();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 1, stream);
+        final byte[] bitmapdata = stream.toByteArray();
+        String bitmapdatas = Base64.encodeToString(bitmapdata, Base64.DEFAULT);
+
+
+//        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+//                .addFormDataPart("image",bitmapdatas)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               20
+//                .addFormDataPart("description", des)
+//                .addFormDataPart("name",MainActivity.mUsername)
+//                .addFormDataPart("email",MainActivity.mUserEmail)
+//                .build();
 
         OkHttpClient client = new OkHttpClient();
 
